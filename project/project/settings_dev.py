2addfe39752348fc0dab64589a6e087f4a5f227c
kyle <kyle@evolphin.com>
2018-07-13 16:40:32 -1000

    undockerized

--- project/project/settings_dev.py
+++ project/project/settings_dev.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'windwardapps_db',
-        'USER': 'windwardapps_db_user',
-        'PASSWORD': 'windwardapps_db_pass',
-        'HOST': 'db',
-        'PORT': 3306,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
     }
 }
 
